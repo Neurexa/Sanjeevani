bbc03d4c0c6f723ed4ec8bcebb2620aa9f3896a5
Somatitoa <102748742+Somatitoa@users.noreply.github.com>
2023-11-18 21:09:53 +0530

    Fix file paths and formatting in wsgi.py and Aside.js

--- frontend/sanjeevani/src/components/Aside.js
+++ frontend/sanjeevani/src/components/Aside.js
@@ -7,30 +7,30 @@ import { useGlobal } from "@/context/GlobalContext";
 import { useRef } from "react";
 
 export default function Aside() {
-    // Global Context
-    const { imageUpload, file } = useGlobal();
+  // Global Context
+  const { imageUpload, file } = useGlobal();
 
-    const imageUploadRef = useRef(null);
-    const handleUploadClick = () => {
-        imageUploadRef.current.click();
-    };
+  const imageUploadRef = useRef(null);
+  const handleUploadClick = () => {
+    imageUploadRef.current.click();
+  };
 
-    return (
-        <div className={styles.aside}>
-            <div className={styles.aside_upper}>
-                <button className={styles.upload_btn} onClick={handleUploadClick}>
-                    <input type="file" name="upload" ref={imageUploadRef} style={{ display: "none" }} onChange={imageUpload} />
-                    <span>Upload</span>
-                    <Image src={upload_icon} alt="Upload" />
-                </button>
-                <p>Upload the photo of the plant which you wish to identify</p>
-            </div>
-            <div className={styles.aside_lower}>
-                <button className={styles.marketinsight_btn}>
-                    <span>Check Market Insights</span>
-                    <Image src={marketinsight_icon} alt="Market Insights" />
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.aside}>
+      <div className={styles.aside_upper}>
+        <button className={styles.upload_btn} onClick={handleUploadClick}>
+          <input type="file" name="upload" ref={imageUploadRef} style={{ display: "none" }} onChange={imageUpload} />
+          <span>Upload</span>
+          <Image src={upload_icon} alt="Upload" />
+        </button>
+        <p>Upload the photo of the plant which you wish to identify</p>
+      </div>
+      <div className={styles.aside_lower}>
+        <button className={styles.marketinsight_btn}>
+          <span>Check Market Insights</span>
+          <Image src={marketinsight_icon} alt="Market Insights" />
+        </button>
+      </div>
+    </div>
+  );
 }
