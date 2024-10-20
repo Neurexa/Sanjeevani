8b662608fac931e4b1359a069c029cb4234a0004
Siddhesh Khairnar <91747482+SIDDHESH1564@users.noreply.github.com>
2024-10-20 14:13:45 +0000

    feat: custom css for camera module

--- backend/sanjeevani/sanjeevani/settings.py
+++ backend/sanjeevani/sanjeevani/settings.py
@@ -28,7 +28,7 @@ DEBUG = False
 ALLOWED_HOSTS = ["*"]
 
 # CORS Settings
-CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
     "http://localhost:3000",
     "https://sanjeevani-frontend.vercel.app",
